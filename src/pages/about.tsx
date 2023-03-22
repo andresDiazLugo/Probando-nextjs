61c617f6fa95c10c98d8767ddde3c5ad35d14371
genjutsu <andresd.ad5@gmail.com>
2023-03-21 23:13:05 -0300

    probando deploy

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import PageLayout from "@/componrnyd/PageLayout"
+export default function about() {
+  return (
+    <PageLayout title="About">
+      <h1>Esta es la página de about</h1>
+    </PageLayout>
+  )
+}
---
+++ src/pages/article/[id].tsx
@@ -0,0 +1,8 @@
+export default function Article() {
+  
+  return (
+    <div>
+        Esto es un articulo...
+    </div>
+  )
+}
